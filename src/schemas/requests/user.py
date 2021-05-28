b78ad87f3eebf55f884371984d90da523c9f8f2b
Sivan Wolberg <sivan@wolberg.pro>
2021-05-28 18:04:28 +0300

    still fix alot of issues #8tzftw

--- src/schemas/requests/user.py
+++ src/schemas/requests/user.py
@@ -3,5 +3,5 @@ from marshmallow import fields, Schema, validate
 
 class CreatePlatformUser(Schema):
     role_names: fields.List(
-        fields.String(required=True, validate=validate.Length(min=3, max=100, error='field name not valid'))
+        fields.Str(required=True, validate=validate.Length(min=3, max=100, error='field name not valid'))
     )
