11a800d532508ab40f64b2dff97d15a92fb88b6a
Sivan Wolberg <sivan@wolberg.pro>
2021-06-01 16:47:15 +0300

    remove debug stuff and dump file that not used

--- config/securiy.py
+++
@@ -1,6 +0,0 @@
-from flask_security import SQLAlchemyUserDatastore
-
-from src.models.roles import Roles
-
-user_datastore = SQLAlchemyUserDatastore(db, User, Roles)
-security = Security(secureApp, user_datastore)
