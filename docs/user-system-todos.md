83db7b065baa933ad7ae5d4883fb9a24f53f0a1f
Sivan Wolberg <sivan@wolberg.pro>
2021-06-07 10:08:56 +0300

    add logic user platform (support only/ or it own user) able to update user info

--- docs/user-system-todos.md
+++ docs/user-system-todos.md
@@ -12,6 +12,6 @@ Task for the user system
   - [ ] add test to at
 - [x] add logic get stores users [filter by: store_code, store name] [ order by store_code , user name ]
   - [ ] add test to at
-- [ ] add logic user platform (support only/ or it own user) able to update user info
+- [x] add logic user platform (support only/ or it own user) able to update user info
   - [ ] add test to at
 
--- src/routes/user.py
+++ src/routes/user.py
@@ -69,7 +69,7 @@ def user_toggle_active(uid):
             return response_error("unknown error", {err: err.cause})
     return response_error("Error on format of the params", {uid: uid})
 
-
+# Todo: Add test for this route
 @current_app.route(settings[os.environ.get("FLASK_ENV", "development")].API_ROUTE.format(route="/user/platform/list/<per_page>/<page>"), methods=["GET"])
 @check_role([RolesTypes.Accounts.value, RolesTypes.Owner.value, RolesTypes.Support.value])
 def get_platform_users(per_page, page):
@@ -79,7 +79,7 @@ def get_platform_users(per_page, page):
     result = userService.query_platform_users(filter, per_page, page)
     return response_success_paging(result.items, result.total, result.pages, result.has_next, result.has_prev)
 
-
+# Todo: Add test for this route
 @current_app.route(settings[os.environ.get("FLASK_ENV", "development")].API_ROUTE.format(route="/user/store/<store_code>/list/<per_page>/<page>"),
                    methods=["GET"])
 @check_role([RolesTypes.Support.value, RolesTypes.StoreAccount.value, RolesTypes.StoreOwner.value, RolesTypes.StoreSupport.value])
